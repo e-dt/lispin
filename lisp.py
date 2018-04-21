a1b00b4b89eb63e035835b41671a80835e606a75
e-dt <punycodedt@gmail.com>
2018-04-22 07:09:29 +1000

    Cleaned up and commented.

--- lisp.py
+++ lisp.py
@@ -1,31 +1,32 @@
-import re
-class Cons:
+class Cons: #Pair object
+    """A pair."""
     def __init__(self, car, cdr):
-        self.car = car
-        self.cdr = cdr
+        self.car = car #first
+        self.cdr = cdr #rest (in linked list)
     def __str__(self):
-        #try
         return "(" + str(self.car) + " . " + str(self.cdr) + ")"
     def __repr__(self):
         return "(" + repr(self.car) + " . " + repr(self.cdr) +")"
 class LispFunction:
+    """A callable lisp function."""
     def __init__(self, lambdaexpr, env):
-        self.lambdaexpr = lambdaexpr
-        self.args = list(iterate(self.lambdaexpr.cdr.car))
-        self.returnv = self.lambdaexpr.cdr.cdr.car
-        self.env = env
+        self.lambdaexpr = lambdaexpr #get body of self
+        self.args = list(iterate(self.lambdaexpr.cdr.car)) #cdr.car (lambda (x) ...) = (x)
+        self.returnv = self.lambdaexpr.cdr.cdr.car #cdr.cdr.car of (lambda (x y) (do x y z...)) is cdr.car of ((x y) (do x y z)) is (do x y z)
+        self.env = env #Le epic lexical scoping    #PLEASE NOTE -- lambdas only take one expression. if you want to use more than one, use a (begin ...)
     def __call__(self, *vals):
         newenv = Environment(self.env, parent=self.env)
-        newenv.update(zip(self.args,vals))
+        newenv.update(zip(self.args,vals)) #defines the arguments' values to be the values passed in
         return apply(self.returnv, newenv)
         
 
 class Environment(dict):
     def __init__(self, *args, **kwargs):
-        self.parent = kwargs.pop('parent')
+        self.parent = kwargs.pop('parent') #parent is the parent environment, e.g. global environment
         dict.__init__(self, *args, **kwargs)
 
     def innermost(self, var):
+        """Gets innermost environment in which `var` appears."""
         return self if var in self else self.parent.innermost(var)
 
 def syntax_sugar(program):
@@ -33,11 +34,11 @@ def syntax_sugar(program):
     index = 0
     while index < len(program):
         val = program[index]
-        if val == "'":
+        if val == "'": #'x -> (quote x)
             new.append(["quote", program[index+1]])
-            index +=1
+            index += 1 #skip next value, as it is already used in the quote expr
         elif type(val) == list:
-            new.append(syntax_sugar(val))
+            new.append(syntax_sugar(val))#recurse
         else:
             new.append(val)
         index += 1
@@ -45,19 +46,22 @@ def syntax_sugar(program):
             
             
 def make_linked_list(iterable):
+    """Makes a linked list out of an iterable."""
     if len(iterable) == 0:
-        return None
+        return None #EMPTY LIST == Nil not (Nil, Nil)
     current = Cons(None, None)
-    a=current
+    start = current #remember the start of the linked list (works because cons are mutable)
     for i in iterable:
         if current.car == None:
             current.car = i
         else:
             current.cdr = Cons(i, None)
             current = current.cdr
-    return a
+    return start
 
-def recursive_mll(iterable): #ONLY TAKES LIST
+def recursive_mll(iterable):
+    "Makes a linked list out of an iterable. Any iterables inside the 
+    iterable are make into linked lists too, and so on."""
     new = []
     for i in iterable:
         if type(i) != list: 
@@ -65,7 +69,7 @@ def recursive_mll(iterable): #ONLY TAKES LIST
         else:
             new.append(recursive_mll(i))
     return make_linked_list(new)
-def iterate(linked):
+def iterate(linked): #makes an iterable out of a linked list. needed because linked lists are just Cons
     class LLIterable:
         def __init__(self, linkedlist):
             self.linkedlist = linkedlist
@@ -73,7 +77,6 @@ def iterate(linked):
             class LLIterator:
                 def __init__(self, ll):
                     self.current = ll
-
                 def __next__(self):
                     if self.current == None:
                         raise StopIteration
@@ -81,15 +84,16 @@ def iterate(linked):
                     self.current = self.current.cdr
                     return this
             return LLIterator(self.linkedlist)
-
     return LLIterable(linked)
 
-def append_to_linkedlist(linked, obj):
-    current = linked
-    while current.cdr != None:
-        current = current.cdr
-    current.cdr = Cons(obj, None)
-    return current
+#NOT CURRENTLY USED, BUT COULD COME IN HANDY
+
+#def append_to_linkedlist(linked, obj):
+#    current = linked
+#    while current.cdr != None:
+#        current = current.cdr
+#    current.cdr = Cons(obj, None)
+#    return current
 
 def tokenise(code_str):
     tokenised = []
@@ -98,7 +102,7 @@ def tokenise(code_str):
         if i == "(":
             if (token_built != ""): tokenised.append(token_built)
             token_built = ""
-            tokenised.append("(")
+            tokenised.append("(") #tokens are represented by themselves for now
         elif i == ")":
             if (token_built != ""): tokenised.append(token_built)
             token_built = ""
@@ -115,14 +119,11 @@ def tokenise(code_str):
 
     return tokenised
 
-
-#LIST ::= ( EXPR )
-#        |( )
-#EXPR ::= ATOM
-#        |LIST
-#        |EXPR EXPR
-#        
+   
 def parse(tokenised):
+    """Turns tokens into objects, parses parens."""
+    #I forgot how this works and why I named this listy.
+    #I was sleep-deprived, okay?
     listy = [[]]
     for token in tokenised:
         if token == "(":
@@ -141,25 +142,24 @@ def parse(tokenised):
     return listy[0]
 
 
-#def e
-def func_n_vals(consappl):
-    func = consappl.car
-    vals = []
-    for i in iterate(consappl.cdr):
-        vals.append(i)
+def func_n_vals(function):
+    """Takes function application and seperates into function and values."""
+    func = function.car
+    vals = [i for i in iterate(function.cdr)] #list comprehensions r fun!
     return func, vals
 
-def apply(consappl,localvars):
-    if consappl.car == 'quote':
-        return consappl.cdr.car
-    func, vals = func_n_vals(consappl)
+def apply(function, localvars):
+    """Applies a function in an environment."""
+    if function.car == 'quote': #required to fix a bug
+        return consappl.cdr.car #i knew why once, but no longer
+    func, vals = func_n_vals(function)
     if func == 'cond':
         for cons in vals:
-            if evaluate(cons.car,localvars) != None:
-                return evaluate(cons.cdr.car,localvars)
-        raise Exception('no default in cond, and it fell through!!')
+            if evaluate(cons.car, localvars) != None:    #None is the only false value; it is also the empty list.
+                return evaluate(cons.cdr.car, localvars) #
+        raise Exception('no default in cond, and it fell through!!') #should this be the behaviour? discuss.
     elif func == 'lambda': 
-        return LispFunction(consappl,localvars)
+        return LispFunction(function, localvars)
     elif func == 'define':
         localvars[vals[0]] = evaluate(vals[1], localvars)
         return localvars[vals[0]]
@@ -179,39 +179,45 @@ def evaluate(expr,localvars):
             return expr
 
 def defaultenv():
+    """Creates a sane environment."""
     env = Environment({}, parent=None)
     def eq(val0, val1):                              #MCCARTHY PAPER VERSION
         if type(val0) == Cons or type(val1) == Cons:
-            raise Exception("eq doesn't take a cons")
-        return "t" if val0 == val1 else None #eval?
+            raise Exception("eq doesn't take a cons") #why not? i don't know, but this is helpful to me
+        return "t" if val0 == val1 else None 
     def begin(*vals):
-        return vals[-1]
+        return vals[-1] #all arguments are evaluated, the last is returned. nice
     env.update([
-        ('eq?', eq), #('eq?', (lambda x,y: x is y), #SCHEME VERSION (CURRENTLY COMMENTED OUT)
+        ('eq?', eq), #('eq?', (lambda x,y: x is y), #SCHEME VERSION
         ('car', (lambda x: x.car)),
         ('cdr', (lambda x: x.cdr)),
         ('cons', (lambda x, y: Cons(x, y))),
         ('atom?', (lambda x: not isinstance(x, Cons))),
         ('begin', begin),
         ('write', (lambda x: print(x) or x)), #dirty hack -- print always returns None :. this always returns x, but prints x first.
+        #MATHS
         ('+', (lambda x, y: x+y)),
         ('-', (lambda x, y: x-y)),
         ('*', (lambda x, y: x*y)),
         ('/', (lambda x, y: x/y))
+        #LOGIC
+        ('and', (lambda x, y: (x != None) and (y != None))), #PLEASE NOTE
+        ('or', (lambda x, y: (x != None) or (y != None))),   #THESE DO NOT SHORT CIRCUIT, AS I DON'T WANT TO WRITE ANOTHER DAMN SPECIAL FORM
+        ('not', (lambda x: "t" if x == None else None))
     ])
     return env
 
 globals = defaultenv()
-def execute(parsed):
+def execute(parsed):         #executes program
+    """Executes program."""
     for i in iterate(parsed):
-        apply(i, globals)
+        apply(i, globals)    #run each lisp.
 
 def run(program):
     execute(syntax_sugar(parse(tokenise(program))))
 
-#((lambda (x) (cond ((eq (quote 1) x) (quote t)) ((eq (quote 1) x) (quote f)))) (quote 1))
-if __name__ == "__main__":
-    test="""
+if __name__ == "__main__": #auto-test
+    test= """
 (define fibonacci
  (lambda (n)
   (cond
