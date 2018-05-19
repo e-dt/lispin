2e58dd0f538565b76ddae1b139584eb1ca5ce444
e-dt <punycodedt@gmail.com>
2018-05-19 18:37:39 +1000

    Started branch vau.

--- lisp.py
+++ lisp.py
@@ -15,12 +15,15 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ##TODO:
+##* hygienic macros using in_env etc.
+##*
 ##* dynamic variables
 ##  pass around a `dynvars` variable
 ##  dynmacros?
 ##  will complicate environment functions, but not much
 ##
 import copy
+from types import FunctionType, BuiltinFunctionType
 def pretty_print(lst):
     new = []
     for i in iterate(lst):
@@ -41,29 +44,36 @@ class Cons: #Pair object
         return "(" + str(self.car) + " . " + str(self.cdr) + ")"
     def __repr__(self):
         return "(" + repr(self.car) + " . " + repr(self.cdr) +")"
-class LispFunction:
+class Vunction:
     """A callable lisp function."""
-    def __init__(self, lambdaexpr, env, macros):
-        self.lambdaexpr = lambdaexpr #get body of self
-        self.args = list(iterate(self.lambdaexpr.cdr.car))
+    def __init__(self, body, env):
+        self.body = body #get body of self
+        self.args = self.body.cdr.car
+        self.dynamic = self.body.cdr.cdr.car
         #cdr.car (lambda (x) ...) = (x)
-        self.returnv = self.lambdaexpr.cdr.cdr.car
+        self.returnv = self.body.cdr.cdr.cdr.car
         #cdr.cdr.car of (lambda (x y) (do x y z...))
         #is cdr.car of ((x y) (do x y z)) is (do x y z)
         self.env = env #Le epic lexical scoping
+        
         #PLEASE NOTE -- lambdas only take one expression.
         #if you want to use more than one, use a (begin ...)
-        self.macros = macros
-    def __call__(self, *vals, inheritedcontext=None):
-        if inheritedcontext != None:
-            env = inheritedcontext
-        else:
-            env = self.env
-        newenv = Environment(env, parent=env)
-        newenv.update(zip(self.args,vals)) #defines the arguments' values to be the values passed in
-        return evaluate(self.returnv, newenv, Environment(self.macros,parent=self.macros))
+        
+    def __call__(self, dynamic, vals):
+        newenv = Environment(self.env, parent=self.env)
+        if type(self.args) == Symbol: #pass cdr function
+            newenv[self.args] = vals
+        elif type(self.args) == Cons:
+            old = newenv
+            newenv.update(zip(iterate(self.args), iterate(vals)))
+        #defines the arguments' values to be the values passed in
+        if self.dynamic != None:
+            newenv[self.dynamic] = dynamic
+        #has extra arg, for caller's env 
+        return evaluate(self.returnv, newenv)
+    
     def __repr__(self):
-        return "func" #debugging
+        return "vunc" #debugging
         
 
 class Environment(dict):
@@ -73,9 +83,15 @@ class Environment(dict):
 
     def innermost(self, var):
         """Gets innermost environment in which `var` appears."""
-        return self if var in self else self.parent.innermost(var)
+        try:
+            return self if var in self else self.parent.innermost(var)
+        except:
+            raise NameError("%s not defined in environment." % var)
 
 class Symbol(str):
+    pass #is the same
+
+class Token(str):
     pass #is the same
 
 def syntax_sugar(program):
@@ -83,7 +99,7 @@ def syntax_sugar(program):
     index = 0
     while index < len(program):
         val = program[index]
-        if val == Symbol("'"): #'x -> (quote x)
+        if val == Token("'"): #'x -> (quote x)
             new.append([Symbol("quote"), program[index+1]])
             index += 1 #skip next value, as it is already used in the quote expr
         elif type(val) == list:
@@ -118,6 +134,7 @@ def recursive_mll(iterable):
         else:
             new.append(recursive_mll(i))
     return make_linked_list(new)
+
 def iterate(linked): #makes an iterable out of a linked list. needed because linked lists are just Cons
     class LLIterable:
         def __init__(self, linkedlist):
@@ -144,39 +161,48 @@ def iterate(linked): #makes an iterable out of a linked list. needed because lin
 #    current.cdr = Cons(obj, None)
 #    return current
 
+def primitive(func):
+    return (type(func) in (FunctionType, BuiltinFunctionType))
+
 def tokenise(code_str):
     tokenised = []
     token_built = ""
     string_mode = False
-    for i in code_str:
+    code_iter = iter(code_str)
+    for i in code_iter:
         if string_mode:
-            if i != '"':
-                token_built += i
-                continue
-            else:
+            if i == '"':
                 string_mode = False
                 continue
-        if i == '"':
+            elif i == "\\":
+                token_built += next(code_iter)
+                continue
+            else:
+                token_built += i
+                continue
+        if i == "\\":
+            token_built = Symbol(token_built + Symbol(next(code_iter)))
+            continue
+        elif i == '"':
             string_mode = True
             continue
-        if i == "(":
+        elif i == "(":
             if (token_built != ""): tokenised.append(token_built)
             token_built = ""
-            tokenised.append(Symbol("(")) #tokens are represented by themselves for now
+            tokenised.append(Token("(")) #tokens are represented by themselves for now
         elif i == ")":
             if (token_built != ""): tokenised.append(token_built)
             token_built = ""
-            tokenised.append(Symbol(")"))
+            tokenised.append(Token(")"))
         elif i == " " or i == "\n":
             if (token_built != ""): tokenised.append(token_built) 
             token_built = ""
         elif i == "'":
             if (token_built != ""): tokenised.append(token_built)
             token_built = ""
-            tokenised.append(Symbol("'"))
+            tokenised.append(Token("'"))
         else:
             token_built = Symbol(token_built + Symbol(i))
-
     return tokenised
 
    
@@ -186,9 +212,9 @@ def parse(tokenised):
     #I was sleep-deprived, okay?
     listy = [[]]
     for token in tokenised:
-        if token == Symbol("("):
+        if token == Token("("):
             listy.append([])
-        elif token == Symbol(")"):
+        elif token == Token(")"):
             listy[-2].append(listy.pop())
         else:
             try:
@@ -202,208 +228,240 @@ def parse(tokenised):
     return listy[0]
 
 
-def func_n_vals(function):
-    """Takes function application and seperates into function and values."""
-    func = function.car
-    vals = [i for i in iterate(function.cdr)] #list comprehensions r fun!
-    return func, vals
-
-def apply(function, localvars, macros):
+def apply(function, environment):
     """Applies a function in an environment."""
-    global startvars
+    func = function.car
+    vals = function.cdr
     try:
-        function = macroexpand(function, localvars, macros)
+        first = function.cdr.car
     except:
-        pass
-    if function.car == Symbol('quote'): #required to fix a bug
-        return function.cdr.car #maybe? unnecessary but who knows
-    func, vals = func_n_vals(function)
+        pass #0arg func
+    try:
+        second = function.cdr.cdr.car
+    except:
+        pass #1arg func
     if func == Symbol('cond'):
-        for cons in vals:
-            if evaluate(cons.car, localvars, macros) != None:    #None is the only false value; it is also the empty list.
-                return evaluate(cons.cdr.car, localvars, macros) #
+        for cons in iterate(vals):
+            if evaluate(cons.car, environment) != None:    #None is the only false value; it is also the empty list.
+                return evaluate(cons.cdr.car, environment) 
         raise Exception('no default in cond, and it fell through!!') #should this be the behaviour? discuss.
-    elif func == Symbol('lambda'): 
-        return LispFunction(function, localvars, macros)
+    
+    elif func == Symbol('quote'):
+        return first
+    
+    elif func == Symbol('vau'):
+        return Vunction(function, environment)
+    
     elif func == Symbol('define'):
-        localvars[vals[0]] = evaluate(vals[1], localvars, macros)
-        return localvars[vals[0]]
+        environment[first] = evaluate(second, environment)
+        return environment[first]
+    
     elif func == Symbol('set!'):
-        localvars.innermost(vals[0])[vals[0]] = evaluate(vals[1], localvars, macros)
-        return localvars.innermost(vals[0])[vals[0]]
-    elif func == Symbol('defmacro'):
-        macros[vals[0]] =  evaluate(vals[1], localvars, macros)
+        environment.innermost(first)[first] = evaluate(second, environment)
+        return environment.innermost(first)[first]
+    
     elif func == Symbol('current-env'):
-        return (copy.deepcopy(localvars),#here the mutable nature of dicts
-                copy.deepcopy(macros))   #works against us, requiring a copy
-    elif func == Symbol('let-env'):
-        newvars, newmacros = evaluate(vals[0], localvars, macros)
-        a=LispFunction(recursive_mll(["lambda", [[]], vals[1]]),
-                       newvars, newmacros)
-        return a()
-    elif func == Symbol('set-env!'):
-        newvars, newmacros = evaluate(vals[0], localvars, macros)
-        if localvars is startvars: #i.e. if top-level (dicts mutable)
-            startvars = newvars
-        localvars = newvars
-        macros = newmacros
+        return copy.deepcopy(localvars)#here the mutable nature of dicts works against us, requiring a copy
+    
+    elif func == Symbol('eval'):
+        expr = evaluate(first, environment)
+        env = evaluate(second, environment)
+        return evaluate(expr, env)
+    elif func == Symbol('load'):
+        execute(syntax_sugar(parse(tokenise(open(first).read()))))
         return None
     else: #defined
-        func = evaluate(func, localvars, macros)
-        vals = [evaluate(val, localvars, macros) for val in vals]
-        return func(*vals) #for builtins
-def evaluate(expr,localvars,macros):
+        func = evaluate(func, environment)
+        #vals not evaluated; vau
+        
+        if primitive(func):
+            if 'operative' in dir(func):      #little bit of a hack to allow use
+                return func(environment, vals)#of builtin functions. its ok tho
+            
+            else: #these are the primitive functions.
+                evaluateds = [evaluate(i, environment) for i in iterate(vals)]
+                return func(*evaluateds)
+            
+        return func(environment, vals) 
+
+def evaluate(expr, environment):
+    """Evaluates an expression in an environment."""
     if type(expr) == Cons:
-        return apply(expr,localvars, macros)
+        return apply(expr, environment)
     else:
         if type(expr) == Symbol:
-            #print(expr)
-            return localvars.innermost(expr)[expr]#variables!
+            return environment.innermost(expr)[expr]#variables!
         else:
             return expr
-def macroexpand(expr, localvars, macros):
-    func, vals = func_n_vals(expr)
-    macro = macros.innermost(func)[func]
-    return macro(*vals)
+
 def defaultenv():
     """Creates a sane environment."""
     env = Environment({}, parent=None)
-    def eq(val0, val1):                              #MCCARTHY PAPER VERSION
+    def wrap(environment, args):
+        func = args.car
+        func = evaluate(func, environment)
+        def newfunc(*vals):
+            evaluateds = make_linked_list(
+                    [evaluate(i, environment) for i in (vals)])
+            if primitive(func) and 'operative' not in dir(func):
+                return func(*evaluateds)
+            else:
+                return func(environment, evaluateds)
+        return newfunc
+
+    wrap.operative = True
+
+    #NORMAL FUNCTIONS
+    
+    def eq(val0, val1):                               #MCCARTHY PAPER VERSION
         if type(val0) == Cons or type(val1) == Cons:
             raise Exception("eq doesn't take a cons") #why not? i don't know, but this is helpful to me
-        return Symbol("t") if val0 == val1 else None 
+        return Symbol("t") if val0 == val1 else None
+
     def begin(*vals):
-        return vals[-1] #all arguments are evaluated, the last is returned. nice
+        return vals[-1]
+    
     def lst(*vals):
         return make_linked_list(vals)
+    
     def set_parent(environment, parentenv):
-        environment[0].parent, environment[1].parent = parentenv
+        environment.parent, environment[1].parent = parentenv
         return environment
+    
     def modify_parent(environment, parentenv):
         newenv = copy.deepcopy(environment)
-        newenv[0].parent, newenv[1].parent = parentenv
+        newenv.parent, newenv[1].parent = parentenv
         return newenv
+    
     def set_env(environment, a, b):
-        environment[0].innermost(a)[a] = b
+        environment.innermost(a)[a] = b
         return environment
+    
     def modify_env(environment, a, b):
         newenv = copy.deepcopy(environment)
-        newenv.innermost[0](a)[a] = b
+        newenv.innermost(a)[a] = b
         return newenv
+    
     def define_env(environment, a, b):
-        environment[0][a] = b
+        environment[a] = b
         return environment
+    
     def defmodify_env(environment, a, b):
         newenv = copy.deepcopy(environment)
-        newenv[0][a] = b
+        newenv[a] = b
         return environment
-    #cant be arsed to do macro env functions rn, TODO
+    
     def in_envp(environment, a):
         try:
             environment[0].innermost(a)
             return Symbol("t")
         except:
             return None
+        
     def in_top_envp(environment, a):
         try:
             environment[0][a]
             return Symbol("t")
         except:
             return None
-    env.update([(Symbol(i[0]),i[1]) for i in [
+
+
+        
+    env.update([(Symbol(i[0]),i[1]) for i in
+    [
+        ('wrap', wrap),
+        
         ('eq?', eq), #('eq?', (lambda x,y: x is y), #SCHEME VERSION
+                    
         ('car', (lambda x: x.car)),
+                    
         ('cdr', (lambda x: x.cdr)),
+                    
         ('cons', (lambda x, y: Cons(x, y))),
-        ('atom?', (lambda x: not isinstance(x, Cons))),
+                    
+        ('atom?', (lambda x: Symbol('t') if (not isinstance(x, Cons)) else None)),
+
+        ('pair?', (lambda x: Symbol('t') if isinstance(x, Cons) else None)),
+
+        ('combiner?', (lambda x: Symbol("t") if callable(x) else None)),
+                    
         ('begin', begin),
+                    
         ('exit', (lambda x=None: exit() if x == None else exit(x))),
+                    
         ('write', (lambda x: print(x) or x)), #dirty hack -- print always returns None :. this always returns x, but prints x first.
-        #ENV
-        ('environment?', (lambda x: Symbol("t") if\
-                          ((type(x[0]) == Environment) and\
-                          (type(x[1]) == Environment) and\
-                           (type(x) == tuple)) else None)),
+
+        #####           
+        #ENV#
+        #####      
+        ('environment?', (lambda x: Symbol("t") if isinstance(x, Environment) else None)),
+                    
         ('set-parent!', set_parent),
+                    
         ('modify-parent', modify_parent),
+                    
         ('parent', (lambda x: (x[0].parent, x[1].parent))),
+                    
         ('parent?', (lambda x, y: Symbol("t") if ((x[0].parent is y[0]) and (x[1].parent is y[1])) else None)),
+                    
         ('set-in-env!', set_env),
+                    
         ('modify-in-env', modify_env),
+                    
         ('define-in-env!', define_env),
+                    
         ('modify-define-in-env', defmodify_env),
+                    
         ('in-env', (lambda x, y: x.innermost(y)[y])),
+                    
         ('in-env?', in_envp),
+                    
         ('in-top-env?', in_top_envp),
-        #MATHS
+
+        #######            
+        #MATHS#
+        #######           
         ('+', (lambda x, y: x+y)),
+                    
         ('-', (lambda x, y: x-y)),
+                    
         ('*', (lambda x, y: x*y)),
+                    
         ('/', (lambda x, y: x/y)),
-        #LOGIC
-        ('and', (lambda x, y: (x != None) and (y != None))), #PLEASE NOTE NONTY SPECIAL FORM
-        #TODO: write as macro !
+
+        #######            
+        #LOGIC#
+        #######            
         ('not', (lambda x: Symbol("t") if x == None else None)),
+
+        #######
+        #MISC.#
+        #######
         ('number?', (lambda x: type(x) == int or type(x) == float)),
-        #LISTS
+
+        #######
+        #LISTS#
+        #######
         ('list', lst)
     ]])
-    #MACROS
-    macros = Environment({}, parent = None)
-    #Hard to have builtin macros because hard to eval in builtin macros
-    return env, macros
+    return env
 
-startvars, macros = defaultenv()
-def execute(parsed):          #executes program
+defaultenv = defaultenv()
+def execute(parsed, env):          #executes program
     """Executes program."""
     for i in iterate(parsed):
-        evaluate(i, startvars, macros)    #run each lisp.
+        evaluate(i, env)    #run each lisp.
 
 def run(program):
-    execute(syntax_sugar(parse(tokenise(program))))
+    execute(syntax_sugar(parse(tokenise(program))),defaultenv)
 
 if __name__ == "__main__": #auto-test
     test ="""
-(write "hello")
-(defmacro or
- (lambda (x y)
-  ((lambda (t)
-   (list 'cond
-    (list t t)
-    (list '(quote t) y)))
-  x)
- )
-)
-(define fibonacci
- (lambda (n)
-  (cond
-   ((eq? n 1) 0)
-   ((eq? n 2) 1)
-   ('t (+ (fibonacci (- n 1)) (fibonacci (- n 2))))
-  )
- )
-)
-(write 'fibonacci-test-7)
-(write (fibonacci 7))
-(write 'macro-or-test-2)
-(write ((lambda (t) (or t 2)) '()))
-(write 'evaluation-test-x)
-(write ((lambda (x) x) 'x)) 
-(write 'let-env-basic-test-2)
-(let-env (current-env) (write ((lambda (t) (or '() t)) 2)))
-(define x 2)
-(define env (current-env))
-(write 'set-env!-basic-test-2)
-(set-env! env)
-(write x)
-(define env (current-env))
+(write "basic test")
+(write ((vau (x y z) env (eval x env)) 1 2 3))
+(write ((wrap (vau (x) env x)) wrap))
 (define x 1)
-(set-env! env)
-(write 'set-env!-advanced-test-2)
 (write x)
-(define env (current-env))
-(write 'parent-env-test-t)
-(write (parent? env (parent env)))
-(exit 0)
+
 """
     run(test)
